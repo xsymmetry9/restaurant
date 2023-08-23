a5878d2890dff796ad191dfc0f60e6c153725a29
Gary <glswim@gmail.com>
2023-08-23 23:13:44 +0800

    fixed code

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 module.exports = {
-    mode: "development",
+    mode: "production",
     entry: "/src/pages/index.js",
     devtool: 'inline-source-map',
     output: {
